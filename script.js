89be7abf5c0e37c79ae5130dfe15b53c50b593bc
Lame Impala <30834126+lame-impala-life@users.noreply.github.com>
2017-10-20 21:08:50 -0500

    Updated initial framebuffer, added bits for button response

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-	// NOT YET
+
+  // PROGRAMMING CONTROLLER
+
+  // m -> PROGRAMMING MODE
+  // 0 = patch/memory select
+  // 1 = edit
+  // 2 = function control
+  var m = 0;
+
+  // BUTTON HANDLER
+  $(".b").click(function (e) {
+    bf[e.target.id]();
+  });
+
+  // BUTTON FUNCTION ASSIGNMENTS
+  var bf = {
+    b1 : function () {/*DECREASE*/},
+    b2 : function () {/*INCREASE*/},
+    b3 : function () {/*STORE*/},
+    b4 : function () {/*MEMORY PROTECT INT*/},
+    b5 : function () {/*MEMORY PROTECT CART*/}
+  }
+
+
+
 });
