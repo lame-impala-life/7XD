1ac921c31fcb8f6c4272f415b332cef97b68f424
Lame Impala <30834126+lame-impala-life@users.noreply.github.com>
2017-10-20 21:39:18 -0500

    Vague Update

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 $(document).ready(function() {
 
-  // PROGRAMMING CONTROLLER
+  // CURRENT PARAMETER
+  var cp;
+  // FRAMEBUFFER UPDATE
+  function fbu (fb) {
+    $("#mm").html(fb);
+  }
 
-  // m -> PROGRAMMING MODE
-  // 0 = patch/memory select
-  // 1 = edit
-  // 2 = function control
-  var m = 0;
+  // READ AS TOGGLE
+  function rat (tv) {
+    if (tv % 2 == 1) {return "ON";}
+    else {return "OFF";}
+  }
 
   // BUTTON HANDLER
   $(".b").click(function (e) {
     bf[e.target.id]();
   });
 
+  // m -> PROGRAMMING MODE (0=MEMORY/1=EDIT/2=FUNCTION)
+  var m = 0;
+
+  // MEMORY PROTECT (0=OFF/1=ON)
+  var mpi = 1; //INTERNAL
+  var mpc = 1; //CARTRIDGE
+
   // BUTTON FUNCTION ASSIGNMENTS
   var bf = {
-    b1 : function () {/*DECREASE*/},
-    b2 : function () {/*INCREASE*/},
+    b1 : function () {cp -= 1;},
+    b2 : function () {cp += 1;},
     b3 : function () {/*STORE*/},
-    b4 : function () {/*MEMORY PROTECT INT*/},
-    b5 : function () {/*MEMORY PROTECT CART*/}
+    b4 : function () {
+      cp = mpi; // STOPPED HERE, 2017 10 20
+      fbu("MEMORY PROTECT<br>INTERNAL="+rat(mpi));
+    },
+    b5 : function () {fbu("MEMORY PROTECT<br>CARTRIDGE="+rat(mpc));}
   }
 
 
